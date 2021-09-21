280f14ac0cc1c90f53a62ed3a200e2f1bcb48346
Brian Faust <hello@basecode.sh>
2021-09-21 09:30:16 +0300

    refactor: use `bcrypto` encodings

--- source/index.test.ts
+++ source/index.test.ts
@@ -1,9 +1,9 @@
 import { decode, encode } from "./index";
 
 test("#encode", () => {
-  expect(encode("f")).toStrictEqual("CO======");
+  expect(encode("f")).toStrictEqual("co");
 });
 
 test("#decode", () => {
-  expect(decode("CO======").toString("utf8")).toStrictEqual("f");
+  expect(decode("co").toString("utf8")).toStrictEqual("f");
 });
--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,7 @@
-import { base32hex } from "rfc4648";
+import { encoding } from "bcrypto";
 
 export const encode = (content: string): string =>
-  base32hex.stringify(Buffer.from(content));
+  encoding.base32.encodeHex(Buffer.from(content));
 
 export const decode = (content: string): Buffer =>
-  Buffer.from(base32hex.parse(content));
+  encoding.base32.decodeHex(content);
